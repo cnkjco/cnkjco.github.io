9177b05d0ec7d13ddca6103c5205b7e3613e4745
cnkjco <cnkjco@gmail.com>
2025-03-06 20:42:54 +0800

    Site updated: 2025-03-06 20:42:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/06/hello-world/","2025/03/06/这是一篇宝隆新文章/"];function toRandomPost(){
+var posts=["2025/03/06/这是一篇宝隆新文章/","2025/03/06/hello-world/","2025/03/06/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
